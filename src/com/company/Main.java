2cd88367954ec4e6c45039bf924710fe741348d6
AidaSofic <ssalihbegovic.aida@gmail.com>
2016-03-20 18:23:15 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+//Write a single integer number that is the sum of all integer numbers lying between 1 and N inclusive.
+
+package com.company;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+       int N;
+        
+       /* int N = 1;
+        int sum = 0;
+        while (N <= 10000){
+            sum = sum + N;
+            ++N;
+        }
+        System.out.print(sum);*/
+
+
+
+}}
